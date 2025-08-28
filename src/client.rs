e84a9c370c1f5dbb932f1c113a8812020149462d
jcoureux <pendragkore@outlook.com>
2025-08-28 14:53:30 +0200

    fix code

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,4 @@
+use crate::maze::MAP1;
 use crate::player_movement;
 use crate::prompts::{prompt_server_addr, prompt_username};
 use crate::structs::{FpsCache, FpsText, Player};
@@ -52,30 +53,7 @@ fn setup(mut commands: Commands) {
                 ..default()
             },
             FpsText,
-                // === Minimap ===
-    let tile_size = 8.0; // each cell = 10x10 pixels
-    let offset_x = -590.0; // shift minimap left
-    let offset_y = 250.0;  // shift minimap up
-
-    for (row, line) in MAP1.iter().enumerate() {
-        for (col, &cell) in line.iter().enumerate() {
-            let color = if cell == 1 {
-                Color::BLACK
-            } else {
-                Color::WHITE
-            };
-            commands.spawn(Sprite {
-                color,
-                custom_size: Some(Vec2::splat(tile_size)),
-                ..default()
-            })
-            .insert(Transform::from_xyz(
-                offset_x + col as f32 * tile_size,
-                offset_y - row as f32 * tile_size,
-                -55.0,  // z = y ?????
-            ));
-        }
-    }
+            
         ));
 
     // === Minimap ===
@@ -98,7 +76,7 @@ fn setup(mut commands: Commands) {
             .insert(Transform::from_xyz(
                 offset_x + col as f32 * tile_size,
                 offset_y - row as f32 * tile_size,
-                -55.0,  // z = y ?????
+                -55.0, 
             ));
         }
     }
